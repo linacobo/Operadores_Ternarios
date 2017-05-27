dcfc50a34124b20537db537f31cd805c6c508ae3
lina cobo <linacobo1828@gmail.com>
2017-05-27 12:40:52 -0500

    Operadores ternarios

---
+++ ejemplo.js
@@ -0,0 +1,13 @@
+var edad=17;
+var conducir=18;
+
+
+if (edad>=18) {
+	console.log("puede conducir");
+}
+else
+{
+	console.log("no puede conducir");
+}
+
+conducir>edad ? console.log("puede conducir") : console.log("no puede conducir");
